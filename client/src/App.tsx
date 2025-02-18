9bd5f6bcb74f6293fac84594a19a8f8d90b3fc36
d.kolomeets <gamepopins@gmail.com>
2025-02-18 21:55:24 +0400

    add unity iframe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,56 @@ import { Spawn } from "./components/Spawn";
 import { useState } from "react";
 import { ControllerConnector } from "@cartridge/connector";
 import { RpcProvider } from "starknet";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { Play } from "./components/Play";
 
 function App() {
   const [controller, setController] = useState<ControllerConnector | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   const provider = new RpcProvider({ nodeUrl: "https://api.cartridge.gg/x/testBrentimus/katana" });
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/loader.js",
+    dataUrl: "build/data.unityweb",
+    frameworkUrl: "build/framework.js.unityweb",
+    codeUrl: "build/wasm.unityweb",
+  }); 
 
   return (
-      <StarknetProvider>
-        <ConnectWallet setController={setController} />
-        {controller && <Spawn controller={controller} provider={provider} />} 
-      </StarknetProvider>
-      
+    <StarknetProvider>
+      <div style={{ display: 'flex' }}>
+        <div style={{ 
+          position: 'fixed', 
+          left: 0, 
+          top: 0,
+          padding: '20px',
+          zIndex: 2,
+          marginRight: '500px'
+        }}>
+          <ConnectWallet setController={setController} />
+          {controller && (
+            <>
+              <Spawn controller={controller} provider={provider} />
+              <Play controller={controller} setIsPlaying={setIsPlaying} />
+            </>
+          )}
+        </div>
+        {isPlaying && (
+          <Unity 
+            unityProvider={unityProvider} 
+            style={{
+              width: '60vw',
+              height: '60vw',
+              maxHeight: '90vh',
+              maxWidth: '90vh',
+              marginLeft: '500px',
+              marginRight: '20px',
+              marginTop: '80px',
+              zIndex: 1
+            }}
+          />
+        )}
+      </div>
+    </StarknetProvider>
   );
 }
 
