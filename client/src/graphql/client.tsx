d4ca85d58632a951595a6a4abc57eebc90764f8b
d.kolomeets <gamepopins@gmail.com>
2025-02-25 21:24:28 +0400

    update unity and config slot

--- client/src/graphql/client.tsx
+++ client/src/graphql/client.tsx
@@ -4,11 +4,11 @@ import { createClient } from 'graphql-ws'
 import { getMainDefinition } from '@apollo/client/utilities'
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:8080/graphql',
+  uri: 'https://api.cartridge.gg/x/mammo-checkers/torii/graphql',
 })
 
 const wsLink = new GraphQLWsLink(createClient({
-    url: 'ws://localhost:8080/graphql',
+    url: 'wss://api.cartridge.gg/x/mammo-checkers/torii/graphql',
     on: {
       connected: (socket, payload) => {
         console.log('WS connected:', payload)
