f4eeeb0e6ff4eb4e9212feb05ee202e0633385d6
Dragon <104348282+icodragon@users.noreply.github.com>
2025-02-28 17:59:40 +1000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		https: {
-			key: fs.readFileSync("localhost-key.pem"),
-			cert: fs.readFileSync("localhost.pem"),
-		},
-	},
+  plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173, 
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      protocol: 'wss', // WebSocket Secure для HMR через Cloudflare
+      host: 'mammo-checkers.mammoblocks.io',
+    },
+  },
 });
